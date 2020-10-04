f4b9be794210ef19e03237ad89d4d3309412e6e9
murraycoding <67034749+murraycoding@users.noreply.github.com>
2020-10-04 15:22:32 -0400

    Final submitted project

--- data_analysis.py
+++ data_analysis.py
@@ -1,29 +1,67 @@
 import numpy as np
+from numpy.core.fromnumeric import mean
 import pandas as pd
+from pandas.core.arrays import integer
 
-# reads the data and puts it into a dataframe
-grades_df = pd.read_csv('grades.csv')
 
-print(grades_df.columns)
+# Step 2: Perform data preparation and cleaning
 
-# creates a new dataframe to put data into instead of transforming the old one
-new_grades_df = pd.DataFrame()
+# Load the data set into a dataframe using Pandas
+performance_df = pd.read_csv('StudentsPerformance.csv')
 
-# setting up new data frame and extracting just the data I need
-new_grades_df['gender'] = grades_df['Gender']
-new_grades_df['student_name'] = grades_df['Student Name']
-new_grades_df['current_course'] = grades_df['Current Course']
-new_grades_df['credits'] = grades_df['Number of Credits']
-new_grades_df['pre_algebra'] = grades_df['DSA Avg.']
-new_grades_df['algebra_1'] = grades_df['DSA Avg..1']
-new_grades_df['algebra_2'] = grades_df['DSA Avg..2']
-new_grades_df['honors_algebra_2'] = grades_df['DSA Avg..3']
-new_grades_df['geoemetry'] = grades_df['DSA Avg..4']
-new_grades_df['honors_geometry'] = grades_df['DSA Avg..5']
-new_grades_df['math_apps'] = grades_df['DSA Avg..6']
-new_grades_df['personal_finance'] = grades_df['DSA Avg..7']
-new_grades_df['trigonometry'] = grades_df['DSA Avg..8']
-new_grades_df['advanced_algebra'] = grades_df['DSA Avg..9']
-new_grades_df['honors_pre_calculus'] = grades_df['DSA Avg..10']
+# explore the number of columns and rows in the dataset.
+shape = performance_df.shape
+print(f'Rows = {shape[0]}')
+print(f'Columns = {shape[1]}')
+
+print('Header Names:')
+print(performance_df.columns)
+
+# Math Section - Determine how to take out data who score 10 or below.
+print('Math Information')
+math_series = pd.Series(data=performance_df['math score'])
+print(f'Average Math Score = {math_series.mean().round(1)}')
+print(f'Range = {math_series.max()-math_series.min()}')
+
+# Reading Section - Determine how to take out data who score 10 or below.
+print('Reading Information')
+reading_series = pd.Series(data=performance_df['reading score'])
+print(f'Average Reading Score = {reading_series.mean().round(1)}')
+print(f'Range = {reading_series.max()-reading_series.min()}')
+
+# Writing Section - Determine how to take out data who score 10 or below.
+print('Writing Information')
+writing_series = pd.Series(data=performance_df['writing score'])
+print(f'Average Writing Score = {writing_series.mean().round(1)}')
+print(f'Range = {writing_series.max() - writing_series.min()}')
+
+# demograph info
+demo_info_titles = performance_df.columns[:5]
+print(demo_info_titles)
+
+for title in demo_info_titles:
+    performance_df[title] = performance_df[title].astype('string')
+
+print(performance_df.dtypes)
+
+# gender breakdown of average scores
+print(performance_df.groupby('gender').mean().round(1))
+
+# free/reduced lunch breakdown
+print(performance_df.groupby(['lunch','gender']).mean().round(1))
+
+# parental level of education breakdown
+print(performance_df.groupby('parental level of education').mean().round(1))
+
+# race/ethnicity and gender
+print(performance_df.groupby(['race/ethnicity','gender']).mean().round(1))
+
+# test prep breakdown
+print(performance_df.groupby(['gender','test preparation course']).mean())
+
+# math score influences
+print(performance_df.groupby(['parental level of education','race/ethnicity']).mean())
+
+
 
 
